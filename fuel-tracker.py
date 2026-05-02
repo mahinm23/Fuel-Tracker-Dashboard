b47fcbb687ee2b1f28f9f790fba150bc12046bf4
Mahin <mahinmostafa@hotmail.com>
2026-05-02 14:50:40 +0100

    MPG vs Trips line chart

--- fuel-tracker.py
+++ fuel-tracker.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.title("Fuel Tracker")
 
 # Import the trips database
 df = pd.read_csv("Trips.csv")
 df['Tank'] = df['Tank'].str.split(' ()').str[0]
+df = df.dropna()
 
 st.write("Fuel Tracker Data:")
 st.dataframe(df.head())
 
+fig = px.line(df, x='Travel Date', y='MPG', title='MPG vs trips')
+st.plotly_chart(fig)
