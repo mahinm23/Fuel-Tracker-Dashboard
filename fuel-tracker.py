233eefe0cdd5de99f4b85c4f7d664f3fb8a6db08
Mahin <mahinmostafa@hotmail.com>
2026-05-02 15:23:57 +0100

    Weekly Average bar chart

--- fuel-tracker.py
+++ fuel-tracker.py
@@ -8,9 +8,15 @@ st.title("Fuel Tracker")
 df = pd.read_csv("Trips.csv")
 df['Tank'] = df['Tank'].str.split(' ()').str[0]
 df = df.dropna()
+df['Travel Date'] = pd.to_datetime(df['Travel Date'], format='%d/%m/%Y')
 
 st.write("Fuel Tracker Data:")
 st.dataframe(df.head())
 
-fig = px.line(df, x='Travel Date', y='MPG', title='MPG vs trips', hover_data=['Location', 'Miles'])
-st.plotly_chart(fig)
+line = px.line(df, x='Travel Date', y='MPG', title='MPG vs trips', hover_data=['Location', 'Miles'])
+st.plotly_chart(line)
+
+df_weekly = df.groupby(pd.Grouper(key='Travel Date', freq='W'))[['MPG', 'Miles', 'Cost']].mean().reset_index()
+
+bar_weekly = px.bar(df_weekly, x='Travel Date', y='MPG', title="Weekly Average MPG:")
+st.plotly_chart(bar_weekly)
