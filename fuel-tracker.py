3eda4733e3303e10b9881152759a2b8b84ce069d
mahinm23 <126404273+mahinm23@users.noreply.github.com>
2026-06-24 14:31:38 +0600

    datetime conversion bug fix

--- fuel-tracker.py
+++ fuel-tracker.py
@@ -11,18 +11,25 @@ def load_data(file):
     return df
 
 df = load_data("Trips_cleaned.csv")
+df['Travel Date'] = pd.to_datetime(df['Travel Date'])
 
 st.write("Fuel Tracker Data:")
 if st.checkbox("Show DF Head"):
     st.dataframe(df.head())
 
-line = px.line(df, x='Travel Date', y='MPG', title='MPG vs trips', hover_data=['Location', 'Miles'])
+line = px.line(df, x='Travel Date', y='MPG', 
+               title='MPG vs trips', 
+               hover_data=['Location', 'Miles'])
 st.plotly_chart(line)
 
-per_trip_costs = px.scatter(df, x='Travel Date', y='Cost', title='Cost Per Trip', hover_data=['Location', 'Miles'])
+per_trip_costs = px.scatter(df, x='Travel Date', y='Cost', 
+                            title='Cost Per Trip', 
+                            hover_data=['Location', 'Miles'])
 st.plotly_chart(per_trip_costs)
 
-df_weekly = df.groupby(pd.Grouper(key='Travel Date', freq='W'))[['MPG', 'Miles', 'Cost']].mean().reset_index()
+df_weekly = df.groupby(pd.Grouper(key='Travel Date', freq='W'))\
+    [['MPG', 'Miles', 'Cost']].mean().reset_index()
 
-bar_weekly = px.bar(df_weekly, x='Travel Date', y='MPG', title="Weekly Average MPG:")
+bar_weekly = px.bar(df_weekly, x='Travel Date', y='MPG', 
+                    title="Weekly Average MPG:")
 st.plotly_chart(bar_weekly)
